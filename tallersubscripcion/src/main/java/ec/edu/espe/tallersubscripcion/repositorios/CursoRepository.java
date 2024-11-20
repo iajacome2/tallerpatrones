fb3f5c0e66f190f6a54542ec6b58d94f143bde72
Ivonne Alexandra Jacome Sangoluisa <iajacome2@espe.edu.ec>
2024-11-19 23:06:55 -0500

    delete

--- tallersubscripcion/src/main/java/ec/edu/espe/tallersubscripcion/repositorios/CursoRepository.java
+++
@@ -1,6 +0,0 @@
-package ec.edu.espe.tallersubscripcion.repositorios;
-
-import ec.edu.espe.tallersubscripcion.entidades.Curso;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CursoRepository extends JpaRepository<Curso, Long> {}
